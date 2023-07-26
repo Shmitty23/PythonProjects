eff0b31766a619b62cbb4c3e20f1a7d6220d8517
Shmitty23 <130246015+Shmitty23@users.noreply.github.com>
2023-07-25 21:22:20 -0700

    update
    Processed CSV and GeoJSON files

--- eq_data/eq_world_map.py
+++ eq_data/eq_world_map.py
@@ -4,7 +4,7 @@ import json
 import plotly.express as px
 
 # Reading data as a string and converting it into a python project 
-path = Path('eq_data/eq_data_1_day_m1.geojson')
+path = Path('eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
@@ -16,15 +16,23 @@ path.write_text(readable_contents)
 # Examine all earthquakes in the dataset 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    eq_titles.append(eq_title)
 
 title = 'Global Earthquakes'
-fig = px.scatter_geo(lat=lats, lon= lons, title=title)
+fig = px.scatter_geo(lat=lats, lon= lons, size=mags, title=title,
+        color=mags,
+        color_continuous_scale='viridis',
+        labels={'color': 'Magnitude'},
+        projection='natural earth',
+        hover_name=eq_titles,
+        )
 fig.show()
