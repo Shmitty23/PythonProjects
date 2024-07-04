a5d32fc3eacb797ed6df47c68a5abeb3305c1c02
Michael <mikey.smith39@gmail.com>
2024-07-04 11:54:33 -0700

    update

---
+++ coffeebot.py
@@ -0,0 +1,12 @@
+print("Hello! Welcome to My cafe, what would you like to order?")
+print("\nMocha\nCappucino\nLatte\nAmericano\nIced coffee\n")
+
+userDrinkChoice = input()
+
+print("\nWould you like to try any of our lunch items?")
+print("\nCrispy grilled cheese\nHam & Swiss on Baguette\nTurkey & pesta on ciabatta\nTomato & mozzarella on focaccia\n")
+
+userFoodChoice = input()
+
+print(f"\nThank you! your {userDrinkChoice} and {userFoodChoice} is ready!")
+print("Have a great day!")
