3152527cccbfaae60e7e59589e59cff933f600bb
Michael <mikey.smith39@gmail.com>
2025-02-11 13:03:03 -0700

    Update on todo list
    Finished todo list

--- java/todolist.java
+++ java/todolist.java
@@ -15,9 +15,34 @@ public class todolist {
         return "Task added!";
     }
 
-    public String viewTask(String view) {
+    public void viewTask() {
+        if (toDoArrayList.size() == 0) {
+            System.out.println("There is nothing in your to-do list. Please add a task.");
+            return;
+        }
+
+        System.out.println("Here's everything in your To-do List!");
         for (int i = 0; i < toDoArrayList.size(); i++) {
-            System.out.println("Here's everything in your To-do List!\n" + i + toDoArrayList.get(i));
+            System.out.println((i + 1) + ". " + toDoArrayList.get(i));
+        }
+    }
+
+    public void removeTask() {
+        if (toDoArrayList.size() == 0) {
+            System.out.println("You have nothing in your To-do list to remove.");
+            return;
+        }
+        System.out.println("What task would you like to remove? (number)");
+        viewTask();
+        int userChoice = sc.nextInt();
+        sc.nextLine();
+
+        if (userChoice < 1 || userChoice > toDoArrayList.size()) {
+            System.out.println("Invalid task number. Please try again.");
+        } else {
+            toDoArrayList.remove( userChoice - 1);
+            System.out.println("Your task has been removed!");
+            viewTask();
         }
     }
 
@@ -28,24 +53,26 @@ public class todolist {
 
     public static void main(String[] args) {
         todolist todo = new todolist();
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to your To-do List!");
         while (todo.activeGame == true) {
-            System.out.println("Welcome to your To-do List!");
-
-            if (todo.toDoArrayList.size() == 0) {
-                System.out.println("You need to add your first task to your To-do list!");
-                System.out.println("\nWhat would you like your first task to be?");
-                String firstTask = sc.nextLine();
-
-                todo.toDoArrayList.add(firstTask);
-                System.out.println("Task added to your To-do List!");
-            }
             
-            System.out.println("Please select an option (number).");
+            System.out.println("\nPlease select an option (number).");
             System.out.println("1. Add task \n2. View task \n3. Remove task \n4. Exit To-do List");
 
-            int userOption = sc.nextInt();
-            sc.nextLine();
+            int userOption = todo.sc.nextInt();
+            todo.sc.nextLine();
+
+            if (userOption == 1) {
+                todo.addTask();
+            } else if ( userOption == 2) {
+                todo.viewTask();
+            } else if( userOption == 3) {
+                todo.removeTask();
+            } else {
+                System.out.println("Have a good day completing your tasks!");
+                todo.activeGame = false;
+                todo.sc.close();
+            }
         }
     }
 }
